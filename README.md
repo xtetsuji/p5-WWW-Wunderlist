306b888ccc3b8e6e362b84f5d7ac1e0e934c364c
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2013-08-15 00:12:57 +0900

    Initial commit. minil new; and ALPHA code and documents.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WWW::Wunderlist - It's new $module
+
+# SYNOPSIS
+
+    use WWW::Wunderlist;
+
+# DESCRIPTION
+
+WWW::Wunderlist is ...
+
+# LICENSE
+
+Copyright (C) OGATA Tetsuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+OGATA Tetsuji <tetsuji.ogata@gmail.com>
---
+++ lib/WWW/Wunderlist.pm
@@ -0,0 +1,39 @@
+package WWW::Wunderlist;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::Wunderlist - It's new $module
+
+=head1 SYNOPSIS
+
+    use WWW::Wunderlist;
+
+=head1 DESCRIPTION
+
+WWW::Wunderlist is ...
+
+=head1 LICENSE
+
+Copyright (C) OGATA Tetsuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+OGATA Tetsuji E<lt>tetsuji.ogata@gmail.comE<gt>
+
+=cut
+
