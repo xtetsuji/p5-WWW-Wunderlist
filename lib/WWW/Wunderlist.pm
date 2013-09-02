773b4e8e62363873c7404dd5cf11882071f2f030
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2013-09-03 00:42:15 +0900

    Rewriten on new "Task" and "List" base API.

--- lib/WWW/Wunderlist.pm
+++ lib/WWW/Wunderlist.pm
@@ -4,8 +4,16 @@ use 5.008008; # for JSON::XS
 use strict;
 use warnings;
 
+use Class::Accessor::Lite (
+    new => 0,
+    rw  => [qw(email password endpoint_url)],
+);
+
+use Carp qw(carp croak);
 use LWP::UserAgent;
 use JSON;
+use WWW::Wunderlist::Task;
+use WWW::Wunderlist::List;
 
 use constant ENDPOINT_URL => 'https://api.wunderlist.com';
 use constant DEBUG => $ENV{DEBUG};
@@ -13,25 +21,15 @@ use constant DEBUG => $ENV{DEBUG};
 our $VERSION = "0.01";
 our $UA_NAME = "Perl/WWW::Wunderlist/$VERSION";
 
+# my $wl = WWW::Wunderlist->new( email => YOUR_EMAIL, password => YOUR_PASSWORD );
 sub new {
     my $class = shift;
     my %arg   = @_;
     my $self  = {};
     $self->{email}    = delete $arg{email};
     $self->{password} = delete $arg{password};
+    $self->{endpoint_url} = ENDPOINT_URL; # for deligation.
     return bless $self, $class;
-}
-
-sub email {
-    my $self = shift;
-    $self->{email} = shift if @_;
-    return $self->{email};
-}
-
-sub password {
-    my $self = shift;
-    $self->{password} = shift if @_;
-    return $self->{password};
 }
 
 # accessor of LWP::UserAgent instance
@@ -50,6 +48,7 @@ sub json {
     return $self->{json} = JSON->new->allow_nonref;
 }
 
+# $wl->login()
 sub login {
     my $self = shift;
     my $res = $self->ua->post(
@@ -66,31 +65,28 @@ sub login {
     else {
         warn $res->as_string;
     }
-    $self->{previous_http_response} = $res;
+    $self->{latest_http_response} = $res;
     return $res->is_success;
 }
 
 sub settings {
     my $self = shift;
-    if ( $self->{login_data} && $self->{login_data}->{settings} ) {
+    local $@;
+    if ( eval { $self->{login_data} && $self->{login_data}->{settings} } ) {
         return $self->{login_data}->{settings};
     }
     else {
-        return +{};
+        return +{ error => $@ };
     }
 }
 
-sub login_with_token {
+sub latest_http_response {
     my $self = shift;
-    ...
+    $self->{latest_http_response} = shift if @_;
+    return $self->{latest_http_response};
 }
 
-
-sub previous_http_response {
-    my $self = shift;
-    $self->{previous_http_response} = shift if @_;
-    return $self->{previous_http_response};
-}
+*previous_http_response = \&latest_http_response;
 
 sub logout {
     my $self = shift;
@@ -99,38 +95,50 @@ sub logout {
     return;
 }
 
-### me
-sub me {
-    my $self = shift;
-    
-}
-
+###
 ### tasks
+###
 
+# my @tasks = $wl->get_tasks();
+# return WWW::Wunderlist::Task objects.
 sub get_tasks {
     my $self = shift;
     my $res = $self->ua->get( ENDPOINT_URL . '/me/tasks' );
-    $self->previous_http_response($res);
+    $self->latest_http_response($res);
     if ( $res->is_success ) {
-        # BETA: とりあえずJSON構造をPerlの構造にして返却
-        return $self->json->utf8->decode($res->content);
+        my $collection = $self->json->utf8->decode($res->content);
+        my @tasks;
+        for my $task_data (@$collection) {
+            my $task = WWW::Wunderlist::Task->new($task_data, $self);
+            push @tasks, $task;
+        }
+        return wantarray ? @tasks : \@tasks;
     }
     else {
         if ( DEBUG ) {
-            warn "get_tasks: FAILED\n";
             warn $res->as_string;
         }
+        carp "get_task is failed.";
         return;
     }
 }
 
-sub set_tasks {
+# my $task = $wl->post_task(
+#     list_id => LIST_ID,
+#     title => TITLE_UTF8_STRING,
+#     starred => BOOL, ### optional
+#     due_date => ISO_FORMAT_DATE, ### optional
+# );
+# post task data to server by Wunderlist API.
+# return WWW::Wunderlist::Task object if request is success.
+# but request is not success, return undef.
+sub post_task {
     my $self = shift;
     my %args = @_;
-    my $list_id  = delete $args{list_id} || "inbox";
-    my $title    = delete $args{title};
-    my $starred  = delete $args{starred}; # optional (0 or 1)
-    my $due_date = delete $args{due_date}; # optional (The date is in ISO format. Example: 2012-12-30T06:00:28Z)
+    my $list_id  = $args{list_id} || "inbox";
+    my $title    = $args{title};
+    my $starred  = $args{starred}; # optional (0 or 1)
+    my $due_date = $args{due_date}; # optional (The date is in ISO format. Example: 2012-12-30T06:00:28Z)
     my $res = $self->ua->post(
         ENDPOINT_URL . '/me/tasks',
         { list_id => $list_id,
@@ -138,20 +146,81 @@ sub set_tasks {
           ( defined $starred ? $starred : () ),
           ( defined $due_date ? $due_date : () ), }
     );
-    $self->previous_http_response($res);
+    $self->latest_http_response($res);
     if ( $res->is_success ) {
-        # BETA: とりあえずJSON構造をPerlの構造にして返却
-        return $self->json->utf8->decode($res->content);
+        my $task = WWW::Wunderlist::Task->new(
+            $self->json->utf8->decode($res->content),
+            $self,
+        );
+        return $task;
     }
     else {
         if ( DEBUG ) {
-            warn "set_tasks: FAILED\n";
             warn $res->as_string;
         }
+        carp "post_task is failed.";
+        return;
+    }
+}
+
+###
+### lists
+###
+
+# my @lists = $wl->get_lists();
+# return WWW::Wunderlist::List objects.
+sub get_lists {
+    my $self = shift;
+    my $res = $self->ua->get( ENDPOINT_URL . '/me/lists' );
+    $self->previous_http_response($res);
+    if ( $res->is_success ) {
+        #return $self->json->utf8->decode($res->content);
+        my $collection = $self->json->utf8->decode($res->content);
+        my @lists;
+        for my $list_data (@$collection) {
+            my $list = WWW::Wunderlist::List->new($list_data, $self);
+            push @lists, $list;
+        }
+        return wantarray ? @lists : \@lists;
+    }
+    else {
+        if ( DEBUG ) {
+            warn $res->as_string;
+        }
+        carp "get_lists is failed.";
+        return;
+    }
+}
+
+# my $list = $wl->post_list( title => TITLE_UTF8_STRING );
+# return WWW::Wunderlist::List object.
+sub post_list {
+    my $self = shift;
+    my %args = @_;
+    my $title    = delete $args{title};
+    my $res = $self->ua->post(
+        ENDPOINT_URL . '/me/lists',
+        { title   => $title }
+    );
+    $self->latest_http_response($res);
+    if ( $res->is_success ) {
+        my $list = WWW::Wunderlist::List->new(
+            $self->json->utf8->decode($res->content),
+            $self,
+        );
+        return $list;
+    }
+    else {
+        if ( DEBUG ) {
+            warn $res->as_string;
+        }
+        carp "post_lists is failed.";
+        return;
     }
 }
 
 1;
+
 __END__
 
 =encoding utf-8
@@ -169,11 +238,16 @@ WWW::Wunderlist - Wunderlist API wrapper
     );
     $wl->login()
         or die "login failed.";
-    my $tasks = $wl->get_tasks()
+    my @tasks = $wl->get_tasks()
         or die "get_tasks failed.";
     # title key's value is UTF-8 decoded string.
-    $wl->set_tasks( title => 'Test of add task at' . time() )
+    $wl->post_task( title => 'Test of add task at' . localtime . ' #hashtag')
         or die "set_tasks failed.";
+
+    my @lists $wl->get_lists()
+        or die "get_lists failed.";
+    $wl->post_list( title => 'New List' )
+        or die "post_list failed.";
 
 =head1 DESCRIPTION
 
@@ -186,11 +260,105 @@ Wunderlist API does not offer OAuth authentication.
 Insted, legacy username/password authentication.
 You use this module, give those information to new constructor.
 
+=head1 METHODS
+
+=head2 my $wl = WWW::Wunderlist->new( ... )
+
+ my $wl = WWW::Wunderlist->new(
+     email => 'your@example.jp',
+     password => 'xxx'
+ );
+
+It creates Wunderlist API object.
+Give your registered "email" and "password" to argument as key-value pair.
+
+=head2 $wl->login()
+
+Tring login. It object accesses Wunderlist API server for login.
+
+If login is success, it returns true.
+But login is failed, it returns false.
+
+=head2 my $settings = $wl->settings()
+
+It returns settings data which is HASH reference.
+
+You can call this method after "login".
+Before login, It returns undef.
+
+See L<Wunderlist API|https://wunderpy.readthedocs.org/en/latest/wunderlist_api/index.html> for detail.
+
+=head2 my $res = $wl->latest_http_response()
+
+It returns latest HTTP response as HTTP::Response object.
+You can use it for debug and some scene.
+
+=head2 $wl->logout()
+
+Do logout.
+
+=head2 my @tasks = $wl->get_tasks()
+
+It returns all tasks.
+Return value is list of WWW::Wunderlist::Task objects on list context,
+or ARRAY reference which has WWW::Wunderlist::Task objects on scalar context.
+
+See L<WWW::Wunderlist::Task> for detail.
+
+For calling it, $wl->login()ed already.
+
+=head2 my $task = $wl->post_task( ... )
+
+ my $task = $wl->post_task(
+     list_id  => LIST_ID,
+     title    => TITLE_UTF8_STRING,
+     starred  => BOOL, ### optional
+     due_date => ISO_FORMAT_DATE, ### optional
+ );
+
+It posts task data by WWW::Wunderlist API.
+
+If it's post is success, then it returns WWW::Wunderlist::Task object.
+But it's post is failed, then it returns undef.
+
+See L<WWW::Wunderlist::Task> for detail.
+
+For calling it, $wl $wl->login()ed already.
+
+=head2 my @lists = $wl->get_lists()
+
+It returns all lists.
+Return value is list of WWW::Wunderlist::List objects on list context,
+or ARRAY reference which has WWW::Wunderlist::List objects on scalar context.
+
+See L<WWW::Wunderlist::List> for detail.
+
+For calling it, $wl $wl->login()ed already.
+
+=head2 my $list = $wl->post_list( ... )
+
+ my $task = $wl->post_list(
+     title => TITLE_UTF8_STRING,
+ );
+
+It posts list data by WWW::Wunderlist API.
+
+If it's post is success, then it returns WWW::Wunderlist::List object.
+But it's post is failed, then it returns undef.
+
+See L<WWW::Wunderlist::List> for detail.
+
+For calling it, $wl $wl->login()ed already.
+
 =head1 CAUTION
 
 THIS VERSION IS ALPHA RELEASE.
 THIS MODULE IS NOT SUPPORT ALL WUNDERLIST APIs.
 UNDER DEVELOPMENT YET.
+
+=head1 SEE ALSO
+
+L<Wunderlist API|https://wunderpy.readthedocs.org/en/latest/wunderlist_api/index.html>
 
 =head1 LICENSE
 
--- t/00_compile.t
+++ t/00_compile.t
@@ -5,6 +5,8 @@ use lib 'lib';
 
 use_ok $_ for qw(
     WWW::Wunderlist
+    WWW::Wunderlist::Task
+    WWW::Wunderlist::List
 );
 
 done_testing;
